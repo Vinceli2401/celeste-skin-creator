f78a8cf3b70c8e2c2f4b5ad6798e3b7853547041
Vince Li <vinceli2401@gmail.com>
2024-11-24 21:20:21 -0800

    Complete Refactoring (11242024)
    1. Rewritten all codes in functions to improve performance and readability
    2. Compatible with pyinstaller now.
    3. Add the directories from cutscenes and objects from the issue

--- CelesteSkinCreator.py
+++ CelesteSkinCreator.py
@@ -2,32 +2,33 @@ import glob
 import os
 import json
 import shutil
+from PIL import Image, ImageColor
+import sys
 
-from PIL import Image
-from PIL import ImageColor
+# Helper function to handle PyInstaller's dynamic paths
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
-# install all the libraries before using the program!!!
-
-config_file_path = os.path.join(os.getcwd(), 'config.json')
-
+# Load config.json
+config_file_path = resource_path('config.json')
 if not os.path.exists(config_file_path):
-    raise FileNotFoundError(f"Configuration file 'config.json' not found in the root directory.")
+    raise FileNotFoundError(f"Please create config.json in the directory and populate it.")
 
 with open(config_file_path, 'r') as config_file:
     config = json.load(config_file)
 
 skinName = config.get("skinName", "default_skin")
 authorName = config.get("authorName", "default_author")
-dash0 = config.get("dash0", "#FFFFFF")  # Default to white
+dash0 = config.get("dash0", "#FFFFFF")
 dash1 = config.get("dash1", "#FFFFFF")
 dash2 = config.get("dash2", "#FFFFFF")
-
 shirt = ImageColor.getrgb(config.get("shirt", "#FFFFFF"))
 sleeves = ImageColor.getrgb(config.get("sleeves", "#FFFFFF"))
 collar = ImageColor.getrgb(config.get("collar", "#FFFFFF"))
 trousers = ImageColor.getrgb(config.get("trousers", "#FFFFFF"))
 
-# Debug output (optional)
 print(f"Skin Name: {skinName}")
 print(f"Author Name: {authorName}")
 print(f"Dash Colors: {dash0}, {dash1}, {dash2}")
@@ -36,78 +37,111 @@ print(f"Sleeves Color: {sleeves}")
 print(f"Collar Color: {collar}")
 print(f"Trousers Color: {trousers}")
 
-from_directory = "./SkinBase"
-to_directory = "./Skin/" + skinName
-shutil.copytree(from_directory, to_directory)
+from_directory = resource_path("SkinBase")
+to_directory = os.path.join("Skin", skinName)
 
-with open(to_directory + '/everest.yaml', encoding='UTF-8', mode='r') as file:
-    newlines = []
-    for line in file:
-        newlines.append(line.replace('Replace', skinName))
+try:
+    shutil.copytree(from_directory, to_directory)
+except FileExistsError as e:
+    print(f"Error: The destination directory already exists. Please remove it or choose a different skin name.")
+    raise
 
-with open(to_directory + '/everest.yaml', encoding='UTF-8', mode='w') as file:
-    for line in newlines:
-        file.write(line)
+def replace_in_file(file_path, replacements):
+    with open(file_path, encoding='UTF-8', mode='r') as file:
+        lines = file.readlines()
+    with open(file_path, encoding='UTF-8', mode='w') as file:
+        for line in lines:
+            for old, new in replacements.items():
+                line = line.replace(old, new)
+            file.write(line)
 
-with open(to_directory + '/SkinModHelperConfig.yaml', encoding='UTF-8', mode='r') as file:
-    newlines = []
-    for line in file:
-        newlines.append(
-            line.replace('Author_Skin', authorName + "_" + skinName).replace("Dash0", dash0).replace("Dash1",
-                                                                                                     dash1).replace(
-                "Dash2", dash2))
+replace_in_file(
+    os.path.join(to_directory, 'everest.yaml'),
+    {'Replace': skinName}
+)
 
-with open(to_directory + '/SkinModHelperConfig.yaml', encoding='UTF-8', mode='w') as file:
-    for line in newlines:
-        file.write(line)
+replace_in_file(
+    os.path.join(to_directory, 'SkinModHelperConfig.yaml'),
+    {
+        'Author_Skin': f"{authorName}_{skinName}",
+        'Dash0': dash0,
+        'Dash1': dash1,
+        'Dash2': dash2
+    }
+)
 
-if not os.path.exists(to_directory + '/Graphics/' + authorName):
-    os.mkdir(to_directory + '/Graphics/' + authorName)
-    os.mkdir(to_directory + '/Graphics/' + authorName + '/' + skinName)
-os.replace(to_directory + '/Graphics/Author/Skin/Sprites.xml',
-           to_directory + '/Graphics/' + authorName + '/' + skinName + '/Sprites.xml')
-shutil.rmtree(to_directory + '/Graphics/Author')
+graphics_author_path = os.path.join(to_directory, 'Graphics', authorName)
+graphics_skin_path = os.path.join(graphics_author_path, skinName)
+os.makedirs(graphics_skin_path, exist_ok=True)
 
-with open(to_directory + '/Graphics/' + authorName + '/' + skinName + '/Sprites.xml', encoding='UTF-8',
-          mode='r') as file:
-    newlines = []
-    for line in file:
-        newlines.append(line.replace('Author/Skin', authorName + "/" + skinName + '/characters'))
+os.replace(
+    os.path.join(to_directory, 'Graphics', 'Author', 'Skin', 'Sprites.xml'),
+    os.path.join(graphics_skin_path, 'Sprites.xml')
+)
+shutil.rmtree(os.path.join(to_directory, 'Graphics', 'Author'))
 
-with open(to_directory + '/Graphics/' + authorName + '/' + skinName + '/Sprites.xml', encoding='UTF-8',
-          mode='w') as file:
-    for line in newlines:
-        file.write(line)
+replace_in_file(
+    os.path.join(graphics_skin_path, 'Sprites.xml'),
+    {'Author/Skin': f"{authorName}/{skinName}/characters"}
+)
 
-with open(to_directory + '/Dialog/English.txt', encoding='UTF-8', mode='r') as file:
-    newlines = []
-    for line in file:
-        newlines.append(line.replace('Author_Skin', authorName + "_" + skinName).replace('skinName', skinName))
+replace_in_file(
+    os.path.join(to_directory, 'Dialog', 'English.txt'),
+    {
+        'Author_Skin': f"{authorName}_{skinName}",
+        'skinName': skinName
+    }
+)
 
-with open(to_directory + '/Dialog/English.txt', encoding='UTF-8', mode='w') as file:
-    for line in newlines:
-        file.write(line)
+from_characters_path = os.path.join(
+    from_directory, 'Graphics', 'Atlases', 'Gameplay', 'Author', 'Skin', 'characters'
+)
+to_characters_path = os.path.join(
+    to_directory, 'Graphics', 'Atlases', 'Gameplay', authorName, skinName, 'characters'
+)
 
-from_directory = to_directory + '/Graphics/Atlases/Gameplay/Author/Skin/characters'
-to_directory = to_directory + '/Graphics/Atlases/Gameplay/' + authorName + '/' + skinName + '/characters'
-shutil.copytree(from_directory, to_directory)
-shutil.rmtree(to_directory + '/Graphics/Atlases/Gameplay/Author')
+from_cutscene_path = os.path.join(
+    from_directory, 'Graphics', 'Atlases', 'Gameplay', 'Author', 'Skin', 'cutscenes', 'payphone'
+)
+to_cutscene_path = os.path.join(
+    to_directory, 'Graphics', 'Atlases', 'Gameplay', authorName, skinName, 'cutscenes', 'payphone'
+)
 
-images = glob.glob(to_directory + '/Graphics/Atlases/Gameplay/' + authorName + '/' + skinName + '/characters/**/*.png')
+from_object_path = os.path.join(
+    from_directory, 'Graphics', 'Atlases', 'Gameplay', 'Author', 'Skin', 'objects', 'lookout'
+)
+to_object_path = os.path.join(
+    to_directory, 'Graphics', 'Atlases', 'Gameplay', authorName, skinName, 'objects', 'lookout'
+)
+
+shutil.copytree(from_characters_path, to_characters_path)
+shutil.copytree(from_cutscene_path, to_cutscene_path)
+shutil.copytree(from_object_path, to_object_path)
+shutil.rmtree(os.path.join(to_directory, 'Graphics', 'Atlases', 'Gameplay', 'Author'))
+
+images = (
+    glob.glob(os.path.join(to_characters_path, '**', '*.png'), recursive=True) +
+    glob.glob(os.path.join(to_cutscene_path, '**', '*.png'), recursive=True) +
+    glob.glob(os.path.join(to_object_path, '**', '*.png'), recursive=True)
+)
+
+def update_image_colors(image_path, color_mappings):
+    img = Image.open(image_path)
+    width, height = img.size
+    for i in range(width):
+        for j in range(height):
+            data = img.getpixel((i, j))
+            for old_color, new_color in color_mappings.items():
+                if data[:3] == old_color:
+                    img.putpixel((i, j), new_color)
+    img.save(image_path)
+
+color_mappings = {
+    (91, 110, 225): shirt,
+    (63, 63, 116): sleeves,
+    (69, 40, 60): collar,
+    (135, 55, 36): trousers
+}
 
 for image in images:
-    img = Image.open(image)
-    width = img.size[0]
-    height = img.size[1]
-    for i in range(0, width):
-        for j in range(0, height):
-            data = img.getpixel((i, j))
-            if data[0] == 91 and data[1] == 110 and data[2] == 225:  # shirt
-                img.putpixel((i, j), shirt)  # rgb color code for color you want to change
-            if data[0] == 63 and data[1] == 63 and data[2] == 116:  # sleeves
-                img.putpixel((i, j), sleeves)
-            if data[0] == 69 and data[1] == 40 and data[2] == 60:  # collar
-                img.putpixel((i, j), collar)
-            if data[0] == 135 and data[1] == 55 and data[2] == 36:  # trousers
-                img.putpixel((i, j), trousers)
-    img.save(image)
+    update_image_colors(image, color_mappings)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # celeste-skin-creator
+## Running
 Run CelesteSkinCreator.py to generate a new skin.
+
+Edit configurations in config.json.
+## Packaging
+Use this command:
+````
+pyinstaller --onefile --add-data "config.json:." --add-data "SkinBase:SkinBase" CelesteSkinCreator.py
